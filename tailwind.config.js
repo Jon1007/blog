49089ad331ee93bf739eb38c7aa166f5b7ca2879
Jon <106767410+Jon1007@users.noreply.github.com>
2022-09-29 18:03:48 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+- purge: ['./src/**/*.html' /* ... */],
++ content: ['./src/**/*.html' /* ... */],
   future: {
     removeDeprecatedGapUtilities: true,
   },
+
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
